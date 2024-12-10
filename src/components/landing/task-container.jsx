6340745f7eb790f3ed5ec1ac029e0ecf525d143c
Mahdi-Moosavi <mahdi.moosavi@hitaxi.cab>
2024-12-10 23:52:30 +0330

    constants added

--- src/components/landing/task-container.jsx
+++ src/components/landing/task-container.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
-function TaskContainer({ direction }) {
+function TaskContainer({ direction , className}) {
   return (
     <div
       className={`w-[250px] h-[65px]  relative z-40 bg-white rounded-[8px] flex overflow-hidden items-center ${
         direction === "left" ? "" : "flex-row-reverse"
-      }`}
+      } ${className}`}
     >
       <div className="w-[30px] h-full bg-[#F42495] text-white flex justify-center items-center">
         <p className="transform rotate-[-90deg]">یکشنبه</p>
