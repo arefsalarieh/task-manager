f1c831c07f7d4630e8ff088b0754c1d0c7c0e7b1
aref <arefsalarieh@gmail.com>
2024-12-10 16:55:56 +0400

    react-query-custom hook  & router & structure base

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Archive from "./pages/Archive";
+import AddCard from "./pages/AddCard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+
+
+
 function App() {
-  return <h1 className="text-red-500">hello world</h1>;
+  const client = new QueryClient({defaultOptions: {queries: {refetchOnWindowFocus: false, staleTime: 1000*6*5}, mutations:{}}})
+
+  const router = createBrowserRouter([
+    {
+      path:'',
+      element:<Landing/>
+    },
+    {
+      path:'/Archive',
+      element:<Archive/>
+    },
+    {
+      path:'/AddCard',
+      element:<AddCard/>
+    },
+    
+  ])
+
+  return (
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router}/>
+    </QueryClientProvider>
+  )
 }
 
 export default App;
