a99e293333d369fa4a7cd08ea9fe278b7716aa1b
aref <arefsalarieh@gmail.com>
2024-12-11 12:04:04 +0400

    motion

--- src/components/landing/task-container.jsx
+++ src/components/landing/task-container.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion } from 'framer-motion';
 
 /* eslint-disable react/prop-types */
 function TaskContainer({ direction, className }) {
@@ -9,13 +10,16 @@ function TaskContainer({ direction, className }) {
 
       {List.map(item=>{
         return(
-          <div
+          <motion.div
           style={{ transformOrigin: "125px 273px", rotate: `${-item*30 }deg` , translate:'-113px' }}
           className="absolute left-[100px] -top-[56px] z-50"
+          initial={{  width: 0  }}
+          whileInView={{  width:'250px' }}
+          transition={{delay:0.7}}  
         >
           <div
             style={{ rotate: `${item*30 }deg`  }}
-            className={`w-[250px] h-[65px] bg-white rounded-[8px] flex overflow-hidden items-center flex-row-reverse ${
+            className={` h-[65px] bg-white rounded-[8px] flex overflow-hidden items-center flex-row-reverse ${
               direction === "left" ? "" : "flex-row-reverse"
             } ${className}`}
           >
@@ -26,20 +30,23 @@ function TaskContainer({ direction, className }) {
               متن توضیحات متن توضیحات تسک
             </h2>
           </div>
-        </div>
+        </motion.div>
         )
       })}
 
 
       {List.map(item=>{
         return(
-          <div
+          <motion.div
           style={{ transformOrigin: "125px 273px", rotate: `${item*30 }deg` , translate:'100px' }}
           className="absolute left-[100px] -top-[56px] z-50"
+          initial={{  width: 0  }}
+          whileInView={{  width:'250px' }}
+          transition={{delay:0.7}} 
         >
           <div
             style={{ rotate: `${-item*30 }deg`  }}
-            className={`w-[250px] h-[65px] bg-white rounded-[8px] flex overflow-hidden items-center ${
+            className={` h-[65px] bg-white rounded-[8px] flex overflow-hidden items-center ${
               direction === "left" ? "" : "flex-row-reverse"
             } ${className}`}
           >
@@ -50,64 +57,17 @@ function TaskContainer({ direction, className }) {
               متن توضیحات متن توضیحات تسک
             </h2>
           </div>
-        </div>
+        </motion.div>
         )
       })}
+
+
+
     </div>
   )
 
 
-  // return (
-  //   <motion.div
-  //     className={`w-[250px] h-[65px] relative z-40 bg-white rounded-[8px] flex overflow-hidden items-center ${
-  //       direction === "left" ? "origin-right" : "flex-row-reverse origin-left"
-  //     } ${className}`}
-  //     initial={{ rotate: -90, opacity: 0 }}
-  //     animate={{
-  //       rotate: direction === "left" ? [-90, 0, 5, -2, 0] : [90, 0, 5, -2, 0],
-  //       opacity: [0, 1],
-  //     }}
-  //     transition={{
-  //       rotate: {
-  //         duration: 2,
-  //         ease: "easeInOut",
-  //         delay: ContainerDelay,
-  //       },
-  //       opacity: {
-  //         duration: 2,
-  //         ease: "easeInOut",
-  //         delay: ContainerDelay,
-  //       },
-  //     }}
-  //   >
-  //     <motion.div
-  //       className="w-[30px] h-full bg-[#F42495] text-white flex justify-center items-center"
-  //       initial={{ opacity: 0, x: direction === "left" ? -100 : 100 }}
-  //       animate={{ opacity: 1, x: 0 }}
-  //       transition={{
-  //         type: "spring",
-  //         stiffness: 50,
-  //         damping: 25,
-  //         delay: DateDelay,
-  //       }}
-  //     >
-  //       <p className="transform rotate-[-90deg]">یکشنبه</p>
-  //     </motion.div>
-  //     <motion.h2
-  //       className="text-black text-center w-[140px]"
-  //       initial={{ opacity: 0, x: direction === "left" ? -100 : 100 }}
-  //       animate={{ opacity: 1, x: 0 }}
-  //       transition={{
-  //         type: "spring",
-  //         stiffness: 50,
-  //         damping: 25,
-  //         delay: TitleDelay,
-  //       }}
-  //     >
-  //       متن توضیحات متن توضیحات تسک
-  //     </motion.h2>
-  //   </motion.div>
-  // );
+
 }
 
 export default TaskContainer;
