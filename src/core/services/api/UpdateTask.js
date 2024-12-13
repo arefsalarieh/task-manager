28e7b0d5f1806b6588aba46a155dfc16164a43a9
Mahdi-Moosavi <mahdi.moosavi@hitaxi.cab>
2024-12-13 19:54:51 +0330

    main Descibe added

--- src/core/services/api/UpdateTask.js
+++ src/core/services/api/UpdateTask.js
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import supabase from "./supabase";
 
