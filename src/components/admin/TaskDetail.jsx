a0374d38d72cca7fa175bcbad9f3faa133c2b1fd
Mahdi-Moosavi <mahdi.moosavi@hitaxi.cab>
2024-12-14 00:44:11 +0330

    detail added to the landing

--- src/components/admin/TaskDetail.jsx
+++ src/components/admin/TaskDetail.jsx
@@ -1,11 +1,5 @@
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-} from "@nextui-org/react";
+/* eslint-disable react/prop-types */
+import { Modal, ModalContent, ModalHeader, ModalBody } from "@nextui-org/react";
 
 const TaskDetail = ({ isOpen, onOpenChange, detail }) => {
   // console.log(detail?.role)
@@ -18,49 +12,39 @@ const TaskDetail = ({ isOpen, onOpenChange, detail }) => {
         style={{ direction: "rtl" }}
       >
         <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                جزئیات وظایف
-              </ModalHeader>
-              <ModalBody>
-                <div className="flex flex-col gap-3">
-                  <div>
-                    <span className="font-bold ">عنوان: </span>
-                    <span>{detail?.title}</span>
-                  </div>
-                  <div>
-                    <span className="font-bold ">توضیحات کوتاه: </span>
-                    <span>{detail?.describe}</span>
-                  </div>
-                  <div>
-                    <span className="font-bold ">نقش: </span>
-                    <span>{detail?.role}</span>
-                  </div>
-                  <div>
-                    <span className="font-bold ">نهاد: </span>
-                    <span>{detail?.section}</span>
-                  </div>
-                  <div>
-                    <span className="font-bold ">اصلی/فرعی بودن: </span>
-                    <span>{detail?.isMain ? "اصلی" : "فرعی"}</span>
-                  </div>
-                  <div>
-                    <span className="font-bold "> توضیحات اصلی:  </span>
-                    <span>{detail?.mainDescribe}</span>
-                  </div>
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              جزئیات وظایف
+            </ModalHeader>
+            <ModalBody>
+              <div className="flex flex-col gap-3">
+                <div>
+                  <span className="font-bold ">عنوان: </span>
+                  <span>{detail?.title}</span>
                 </div>
-              </ModalBody>
-              {/* <ModalFooter>
-            <Button color="danger" variant="flat" onPress={onClose}>
-              بستن
-            </Button>
-            <Button color="primary" type="submit">
-              ثبت
-            </Button>
-          </ModalFooter> */}
-            </>
-          )}
+                <div>
+                  <span className="font-bold ">توضیحات کوتاه: </span>
+                  <span>{detail?.describe}</span>
+                </div>
+                <div>
+                  <span className="font-bold ">نقش: </span>
+                  <span>{detail?.role}</span>
+                </div>
+                <div>
+                  <span className="font-bold ">نهاد: </span>
+                  <span>{detail?.section}</span>
+                </div>
+                <div>
+                  <span className="font-bold ">اصلی/فرعی بودن: </span>
+                  <span>{detail?.isMain ? "اصلی" : "فرعی"}</span>
+                </div>
+                <div>
+                  <span className="font-bold "> توضیحات اصلی: </span>
+                  <span>{detail?.mainDescribe}</span>
+                </div>
+              </div>
+            </ModalBody>
+          </>
         </ModalContent>
       </Modal>
     </>
--- src/components/landing/task-container.jsx
+++ src/components/landing/task-container.jsx
@@ -1,29 +1,32 @@
+/* eslint-disable react/jsx-key */
 import { useEffect, useState } from "react";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import { useTasks } from "../admin/hooks/useTasks";
-import {Spinner} from "@nextui-org/react";
+import { Spinner, useDisclosure } from "@nextui-org/react";
+import TaskDetail from "../admin/TaskDetail";
 
 /* eslint-disable react/prop-types */
 function TaskContainer({ direction, className }) {
-  const [List, setList] = useState([1, 2, 3, 4, 5]);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [CurrDetail, SetCurrDetail] = useState([]);
   const [LeftList, setLeftList] = useState([]);
-  const [RightList, setRightList] = useState([]);
 
-  const { tasks, isLoading, isError } = useTasks();
+  const OpenDetailHandler = (item) => {
+    onOpen();
+    SetCurrDetail(item);
+  };
+
+  const { tasks, isLoading } = useTasks();
   tasks && console.log(tasks);
 
   useEffect(() => {
-    let leftArr = []
-    let rightArr = []
-
     for (let i = 0; i < tasks?.length; i++) {
-      if(i%2 === 0){
-        setLeftList([...LeftList , tasks[i]])
+      if (i % 2 === 0) {
+        setLeftList([...LeftList, tasks[i]]);
       }
-      
     }
     // tasks?.forEach((item,index)=>{
-    //   if(index%2 === 0){ 
+    //   if(index%2 === 0){
     //     leftArr.push(item)
     //   }else{
     //     rightArr.push(item)
@@ -31,130 +34,92 @@ function TaskContainer({ direction, className }) {
 
     //   setLeftList(leftArr)
     //   setRightList(rightArr)
-      
+
     // })
   }, []);
 
-
-
-
   return (
     <div className="relative  top-12 left-20 w-[441px] h-[452px]  border-2 border-transparent  ">
-      {isLoading &&   <Spinner size="lg" style={{scale:'4'}} className="absolute left-[200px] top-[210px] z-50" />}
-      {tasks?.map((item,index)=>{
-        if(index %2 === 0 && index<10){
-          return(
+      {isLoading && (
+        <Spinner
+          size="lg"
+          style={{ scale: "4" }}
+          className="absolute left-[200px] top-[210px] z-40"
+        />
+      )}
+      {tasks?.map((item, index) => {
+        if (index % 2 === 0 && index < 10) {
+          return (
             <motion.div
-            style={{ transformOrigin: "125px 273px", rotate: `${((index/2)+1)*(-30) }deg` , translate:'-113px' }}
-            className="absolute left-[100px] -top-[56px] z-50"
-            initial={{  width: 0  }}
-            whileInView={{  width:'250px' }}
-            transition={{delay:Math.random()}}  
-          >
-            <div
-              style={{ rotate: `${((index/2)+1)*30 }deg`  }}
-              className={` h-[65px] bg-white rounded-[8px] flex overflow-hidden items-center flex-row-reverse ${
-                direction === "left" ? "" : "flex-row-reverse"
-              } ${className}`}
+              style={{
+                transformOrigin: "125px 273px",
+                rotate: `${(index / 2 + 1) * -30}deg`,
+                translate: "-113px",
+              }}
+              className="absolute left-[100px] -top-[56px] z-40 cursor-pointer"
+              initial={{ width: 0 }}
+              whileInView={{ width: "250px" }}
+              transition={{ delay: Math.random() }}
+              onClick={() => {
+                OpenDetailHandler(item);
+              }}
             >
-              <div className="w-[30px] h-full bg-[#F42495] text-white flex justify-center items-center">
-                <p className="transform rotate-[-90deg]">{item.role}</p>
+              <div
+                style={{ rotate: `${(index / 2 + 1) * 30}deg` }}
+                className={` h-[65px] bg-white rounded-[8px] flex overflow-hidden items-center flex-row-reverse hover:scale-105 transition-all ${
+                  direction === "left" ? "" : "flex-row-reverse"
+                } ${className}`}
+              >
+                <div className="w-[30px] h-full bg-[#F42495] text-white flex justify-center items-center">
+                  <p className="transform rotate-[-90deg]">{item.role}</p>
+                </div>
+                <h2 className="text-black text-center w-[140px]">
+                  {item.title}
+                </h2>
               </div>
-              <h2 className="text-black text-center w-[140px]">
-                {item.title}
-              </h2>
-            </div>
-          </motion.div>
-          )
-        }
-        else if(index %2 === 1 && index<10){
-          return(
+            </motion.div>
+          );
+        } else if (index % 2 === 1 && index < 10) {
+          return (
             <motion.div
-            style={{ transformOrigin: "125px 273px", rotate: `${Math.floor((index/2)+1)*30 }deg` , translate:'100px' }}
-            className="absolute left-[100px] -top-[56px] z-50"
-            initial={{  width: 0  }}
-            whileInView={{  width:'250px' }}
-            transition={{delay:Math.random()}}   
-          >
-            <div
-              style={{ rotate: `${-Math.floor((index/2)+1)*30 }deg`  }}
-              className={` h-[65px] bg-white rounded-[8px] flex overflow-hidden items-center ${
-                direction === "left" ? "" : "flex-row-reverse"
-              } ${className}`}
+              style={{
+                transformOrigin: "125px 273px",
+                rotate: `${Math.floor(index / 2 + 1) * 30}deg`,
+                translate: "100px",
+              }}
+              onClick={() => {
+                OpenDetailHandler(item);
+              }}
+              className="absolute left-[100px] -top-[56px] z-40 cursor-pointer"
+              initial={{ width: 0 }}
+              whileInView={{ width: "250px" }}
+              transition={{ delay: Math.random() }}
             >
-              <div className="w-[30px] h-full bg-[#F42495] text-white flex justify-center items-center">
-                <p className="transform rotate-[-90deg]">{item.role}</p>
+              <div
+                style={{ rotate: `${-Math.floor(index / 2 + 1) * 30}deg` }}
+                className={` h-[65px] bg-white rounded-[8px] flex overflow-hidden items-center hover:scale-105 transition-all ${
+                  direction === "left" ? "" : "flex-row-reverse"
+                } ${className}`}
+              >
+                <div className="w-[30px] h-full bg-[#F42495] text-white flex justify-center items-center">
+                  <p className="transform rotate-[-90deg]">{item.role}</p>
+                </div>
+                <h2 className="text-black text-center w-[140px]">
+                  {item.title}
+                </h2>
               </div>
-              <h2 className="text-black text-center w-[140px]">
-              {item.title}
-              </h2>
-            </div>
-          </motion.div>
-          )
+            </motion.div>
+          );
         }
- 
       })}
 
-      {/* {LeftList?.map((item , index)=>{
-        return(
-          <motion.div
-          style={{ transformOrigin: "125px 273px", rotate: `${-(index+1)*30 }deg` , translate:'-113px' }}
-          className="absolute left-[100px] -top-[56px] z-50"
-          initial={{  width: 0  }}
-          whileInView={{  width:'250px' }}
-          transition={{delay:Math.random()}}  
-        >
-          <div
-            style={{ rotate: `${(index+1)*30 }deg`  }}
-            className={` h-[65px] bg-white rounded-[8px] flex overflow-hidden items-center flex-row-reverse ${
-              direction === "left" ? "" : "flex-row-reverse"
-            } ${className}`}
-          >
-            <div className="w-[30px] h-full bg-[#F42495] text-white flex justify-center items-center">
-              <p className="transform rotate-[-90deg]">یکشنبه</p>
-            </div>
-            <h2 className="text-black text-center w-[140px]">
-              متن توضیحات متن توضیحات تسک
-            </h2>
-          </div>
-        </motion.div>
-        )
-      })}
-
-
-      {RightList?.map(item=>{
-        return(
-          <motion.div
-          style={{ transformOrigin: "125px 273px", rotate: `${item*30 }deg` , translate:'100px' }}
-          className="absolute left-[100px] -top-[56px] z-50"
-          initial={{  width: 0  }}
-          whileInView={{  width:'250px' }}
-          transition={{delay:Math.random()}}   
-        >
-          <div
-            style={{ rotate: `${-item*30 }deg`  }}
-            className={` h-[65px] bg-white rounded-[8px] flex overflow-hidden items-center ${
-              direction === "left" ? "" : "flex-row-reverse"
-            } ${className}`}
-          >
-            <div className="w-[30px] h-full bg-[#F42495] text-white flex justify-center items-center">
-              <p className="transform rotate-[-90deg]">یکشنبه</p>
-            </div>
-            <h2 className="text-black text-center w-[140px]">
-              متن توضیحات متن توضیحات تسک
-            </h2>
-          </div>
-        </motion.div>
-        )
-      })} */}
-
-
-
+      <TaskDetail
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        detail={CurrDetail}
+      />
     </div>
-  )
-
-
-
+  );
 }
 
 export default TaskContainer;
